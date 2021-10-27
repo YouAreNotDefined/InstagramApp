e5e647f3d569b1c471bed45101e4bc3dbb10e135
YouAreNotDefined <for.1self.ik@gmail.com>
2021-10-27 21:49:00 +0900

    first commit

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -9,9 +9,6 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
   },
   buildModules: [
     '@nuxtjs/vuetify',
